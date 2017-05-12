e6fac006d58aabc5a53e8803fb0900ec9ca9f2d2
somename <someemail>
2017-05-12 11:59:22 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -3,12 +3,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestBowling {
-
+/*
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-	
+	*/
 	@Test
 	public void test_strike(){
 		boolean provera = true;
